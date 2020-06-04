936ceaba29baa6e2f90261514dd0f39fc05b954f
saint1998 <nathabordine.wonghirundecha@agoda.com>
2020-06-04 11:36:08 +0700

    created front structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([{ id: 1, subject: "test" }]);
+
+  async function fetchData() {
+    // const response = await readMail();
+    const response = "";
+    setData(response);
+  }
+
+  async function onClickUpdate(id, subject) {
+    // const response = await updateMail(id, subject);
+    const response = "";
+
+    fetchData();
+  }
+
+  async function onClickDelete(id) {
+    // const response = await deleteMail(id);
+    const response = "";
+
+    fetchData();
+  }
+
+  async function onClickCreate(subject) {
+    // const response = await createMail(subject);
+    const response = "";
+
+    fetchData();
+  }
+
+  useEffect(() => {
+    // fetchData();
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
       <table class="table table-striped">
@@ -9,31 +44,28 @@ function App() {
           <tr>
             <th scope="col">#</th>
             <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Update</th>
+            <th scope="col">Delete</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {data.map((mail) => {
+            return (
+              <tr>
+                <th scope="row">{mail.id}</th>
+                <td>{mail.subject}</td>
+                <td>
+                  <button onClick={onClickUpdate}>Update</button>
+                </td>
+                <td>
+                  <button onClick={onClickDelete}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
+      <button onClick={onClickCreate}>Create</button>
     </div>
   );
 }
